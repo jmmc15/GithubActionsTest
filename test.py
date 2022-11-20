91b1b65ad2786762271511d36d367a30c874bd3f
Jules Miko Cuevas <julesmiko.cuevas.cics@ust.edu.ph>
2022-11-20 17:09:18 +0800

    added py unittest

---
+++ test.py
@@ -0,0 +1,18 @@
+import unittest
+import final
+
+class TestMapQuest(unittest.TestCase):
+    def test_app(self):
+        self.assertEqual(final.json_status, 0)
+
+    def test_api(self):
+        self.assertEqual(final.main_api, "https://www.mapquestapi.com/directions/v2/route?")
+
+    def test_api(self):
+        self.assertIsNotNone(final.key, None)
+       
+
+if __name__ == '__main__':
+    unittest.main()
+
+#if we run this directly run the code within that conidition 
